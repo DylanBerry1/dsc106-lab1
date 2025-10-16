ad896134edda7b9cd7b345e189b4b1bd62e2c190
Dylan Berry <dylanjberry@icloud.com>
2025-10-16 16:09:24 -0700

    steps 1 2 and most of 3

---
+++ global.js
@@ -0,0 +1,38 @@
+// console.log('IT’S ALIVE!');
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/dsc106-lab1/";         // GitHub Pages repo name
+
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'resume/', title: 'Resume'},
+  { url: 'contacts/', title: 'Contacts'}
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  // next step: create link and add it to nav
+  
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
+
+
+//lab 3 setups
+// let navlinks = $$('nav a');
+
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+
+// currentLink?.classList.add('jfkdj');
